fc61f8e86ce314988157b67302f6e3f05488bfef
Simon Wex <simon@simonwex.com>
2012-01-25 01:25:34 -0800

    Giving the home page some content, no layouts here.
    Also including the bookmarklet.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
-// 
-// /*
-//  * GET home page.
-//  */
-// 
-// exports.index = function(req, res){
-//   res.render('index')
-// };
+
+/*
+ * GET home page.
+ */
+
+function getBookmarklet(){
+  var baseCode = "javascript:(function()%7Bvar%20script=document.createElement('script');script.src='http://localhost:3000/javascripts/towtruck.js';script.className='TowTruck';document.head.appendChild(script);%7D)();"
+  //TODO: replace localhost:3000 with the appropriate URL.
+  return baseCode;
+}
+
+exports.index = function(req, res){
+  res.render('index', {bodyClass: 'home', layout: false, bookmarkletHref: getBookmarklet()})
+};
