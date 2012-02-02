790f07cfe0f687b702c728d8294a02d15dbd166f
Simon Wex <simon@simonwex.com>
2012-02-01 22:58:59 -0800

    Redoing vertical sizing.

--- http/public/javascripts/collaborate.js
+++ http/public/javascripts/collaborate.js
@@ -1,17 +1,21 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
- 
+
 $(function(){
   var resizeHandler = function(){
-    $("#etherpad").height($(window).height() - 180);
+    var bodyPadding = parseInt($('body.collaborate').css('padding-top'), 10);
+    var footerHeight = $('footer').height();
+    $("#etherpad, #chat").height($(window).height() - (bodyPadding + footerHeight));
   };
+  
   $(window).resize(resizeHandler);
   resizeHandler();
   
   $('.share-url').click(function(){
     return false;
   });
+  
   $('input.share').click(function(){
     $(this).focus().select();
   });
@@ -19,5 +23,9 @@ $(function(){
   $('li.FileTab a').click(function(){
     $('#file_tab_nav li.FileTab').removeClass('active');
     $(this).parent().addClass('active');
+  });
+  
+  $('#chat form').submit(function(){
+    return false;
   });
 });
