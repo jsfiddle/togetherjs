e42fadfb948c9d1e7ed3b295d85f6c21372c35db
Atul Varma <varmaa@gmail.com>
2012-04-27 09:28:16 -0500

    changed test-noscript.js to use assertParseInfo().

--- test/test-noscript.js
+++ test/test-noscript.js
@@ -12,10 +12,9 @@ test("SCRIPT_ELEMENT_NOT_ALLOWED error is reported", function() {
   var ndb = new Slowparse.NoscriptDOMBuilder(document);
   var html = '<script>alert("yo");</script>';
   var result = Slowparse.HTML(ndb, html);
-  deepEqual(result.error, {
-    type: "SCRIPT_ELEMENT_NOT_ALLOWED",
-    start: 0,
-    end: 7
+  equal(result.error.type, "SCRIPT_ELEMENT_NOT_ALLOWED");
+  assertParseInfo(html, result, "result", {
+    'error': '<script'
   });
 });
 
@@ -23,16 +22,10 @@ test("EVENT_HANDLER_ATTR_NOT_ALLOWED error is reported", function() {
   var ndb = new Slowparse.NoscriptDOMBuilder(document);
   var html = '<p onclick="alert(\'yo\');">hi</p>';
   var result = Slowparse.HTML(ndb, html);
-  deepEqual(result.error, {
-    type: "EVENT_HANDLER_ATTR_NOT_ALLOWED",
-    name: {
-      start: 3,
-      end: 10
-    },
-    value: {
-      start: 11,
-      end: 25
-    }
+  equal(result.error.type, "EVENT_HANDLER_ATTR_NOT_ALLOWED");
+  assertParseInfo(html, result.error, "error", {
+    'name': 'onclick',
+    'value': '"alert(\'yo\');"'
   });
 });
 
@@ -40,15 +33,9 @@ test("JAVASCRIPT_URL_NOT_ALLOWED error is reported", function() {
   var ndb = new Slowparse.NoscriptDOMBuilder(document);
   var html = '<a href="javascript:alert(\'yo\');">hi</a>';
   var result = Slowparse.HTML(ndb, html);
-  deepEqual(result.error, {
-    type: "JAVASCRIPT_URL_NOT_ALLOWED",
-    name: {
-      start: 3,
-      end: 7
-    },
-    value: {
-      start: 8,
-      end: 33
-    }
+  equal(result.error.type, "JAVASCRIPT_URL_NOT_ALLOWED");
+  assertParseInfo(html, result.error, "error", {
+    'name': 'href',
+    'value': '"javascript:alert(\'yo\');"'
   });
 });
