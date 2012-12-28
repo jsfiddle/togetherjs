7bb30d08a16a71e022e3f7fbd5d6fff5118c24f0
Ian Bicking <ianb@colorstudy.com>
2012-12-28 14:46:47 -0600

    Add walkabout.js for self-testing

--- src/towtruck.js
+++ src/towtruck.js
@@ -14,7 +14,8 @@
     "http://localhost:8080/channels.js",
     "http://localhost:8080/towtruck-runner.js",
     "http://localhost:8080/chat.js",
-    "http://localhost:8080/tracker.js"
+    "http://localhost:8080/tracker.js",
+    "http://localhost:8080/libs/walkabout.js/walkabout.js"
     //"http://localhost:8080/libs/sharejs/src/client/microevent.js",
     //"http://localhost:8080/libs/sharejs/src/types/helpers.js",
     //"http://localhost:8080/libs/sharejs/src/client/textarea.js",
