51703af14b94774eb0237d6183c4ad389ac30749
Ian Bicking <ian@ianbicking.org>
2013-01-02 16:18:00 -0500

    Ignore the hash after & (i.e., don't treat as a page name)

--- examples/friendlycode/js/fc/current-page-manager.js
+++ examples/friendlycode/js/fc/current-page-manager.js
@@ -17,14 +17,26 @@ define(function() {
     if (supportsPushState)
       window.history.replaceState({pageToLoad: pageToLoad}, "",
                                   window.location.href);
+
+    function getPageFromHash() {
+      var pageToLoad = window.location.hash.slice(1);
+      if (pageToLoad.indexOf("&") != -1) {
+        pageToLoad = pageToLoad.slice(0, pageToLoad.indexOf("&"));
+      }
+      if (! pageToLoad) {
+        return null;
+      }
+      return pageToLoad;
+    }
     
     // If a URL hash is specified, it should override anything provided by
     // a server.
     if (window.location.hash.slice(1))
-      pageToLoad = window.location.hash.slice(1);
+      pageToLoad = getPageFromHash();
     
     window.addEventListener("hashchange", function(event) {
-      var newPageToLoad = window.location.hash.slice(1);
+      var newPageToLoad = getPageFromHash();
+      console.log("Loading page", newPageToLoad);
       if (newPageToLoad != pageToLoad) {
         pageToLoad = newPageToLoad;
         loadPage(pageToLoad);
@@ -45,10 +57,15 @@ define(function() {
     self.currentPage = function() { return pageToLoad; };
     self.changePage = function(page, url) {
       pageToLoad = page;
-      if (supportsPushState)
+      if (supportsPushState) {
         window.history.pushState({pageToLoad: page}, "", url);
-      else
-        window.location.hash = "#" + page;
+      } else {
+        var rest = "";
+        if (window.location.hash.indexOf("&") != -1) {
+          rest = window.location.hash.substr(window.location.indexOf("&"));
+        }
+        window.location.hash = "#" + page + rest;
+      }
     };
     
     return self;
