ae1e133cb0982bc10046193e256c6da708513697
Ian Bicking <ianb@colorstudy.com>
2013-02-08 13:35:10 -0600

    Fix URLs that point to localhost:8080 in templates.  Copy images and fonts in make-static-client.  Ignore when directory already exists for publish-client

--- app/http/views/towtruck/templates.js
+++ app/http/views/towtruck/templates.js
@@ -2,6 +2,7 @@ define(["util"], function (util) {
   function clean(t) {
     t = t.replace(/[<][%]\s*\/\*[\S\s\r\n]*\*\/\s*[%][>]/, "");
     t = util.trim(t);
+    t = t.replace(/http:\/\/localhost:8080/g, TowTruck.baseUrl);
     return t;
   }
   return {
