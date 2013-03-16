1ad27189dd3be53bbc5b43e4f394299f86163eb9
Ian Bicking <ianb@colorstudy.com>
2013-03-15 21:58:04 -0500

    Added forgotten modal.js

---
+++ app/http/public/towtruck/modal.js
@@ -0,0 +1,57 @@
+define(["util", "jquery", "session"], function (util, $, session) {
+  var assert = util.assert;
+  var modal = util.Module("modal");
+
+  function getBackground() {
+    if (getBackground.element) {
+      return getBackground.element;
+    }
+    var background = $("#towtruck-modal-background");
+    assert(background.length);
+    getBackground.element = background;
+    background.click(function () {
+      modal.stopModal();
+    });
+    return background;
+  }
+
+  modal.showModal = function (el) {
+    el = $(el);
+    assert(el.hasClass("towtruck-modal"));
+    if (! el.find(".towtruck-close").length) {
+      el.prepend($('<div class="towtruck-close">&times;</div>'));
+    }
+    el.find(".towtruck-close, .towtruck-modal-close").click(modal.stopModal);
+    getBackground().show();
+    el.show();
+    bindEscape();
+  };
+
+  function bindEscape() {
+    $(document).keydown(onKeydown);
+  }
+
+  function unbindEscape() {
+    $(document).unbind("keydown", onKeydown);
+  }
+
+  session.on("close", function () {
+    unbindEscape();
+  });
+
+  function onKeydown(event) {
+    if (event.which == 27) {
+      modal.stopModal();
+    }
+  }
+
+  modal.stopModal = function () {
+    getBackground().hide();
+    var active = $(".towtruck-modal:visible");
+    active.find(".towtruck-close, .towtruck-modal-close").unbind("close", modal.stopModal);
+    active.hide();
+    unbindEscape();
+  };
+
+  return modal;
+});
