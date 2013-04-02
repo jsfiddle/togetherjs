af4448a7bf18641f35259a23cda0f13b5262819f
Ian Bicking <ianb@colorstudy.com>
2013-04-02 14:27:27 -0500

    try a different commit env variable

--- app/http/views/towtruck/towtruck.js
+++ app/http/views/towtruck/towtruck.js
@@ -14,7 +14,7 @@
   var baseUrl = "<%= process.env.PUBLIC_BASE_URL %>";
   // FIXME: we could/should use a version from the checkout, at least
   // for production
-  var cacheBust = "<%= process.env.COMMIT_HASH || '' %>" || Date.now();
+  var cacheBust = "<%= process.env.HEROKU_SLUG || '' %>" || Date.now();
 
   // FIXME: I think there's an event that would be called before load?
   // DOMReady?
