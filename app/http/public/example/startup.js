21c0ac582050d36cd070dce8a1b3a2c011a89f48
Ian Bicking <ianb@colorstudy.com>
2013-04-09 13:28:36 -0500

    Update the .active class on the indicators that are outside the carousel

--- app/http/public/example/startup.js
+++ app/http/public/example/startup.js
@@ -26,3 +26,14 @@ $(function () {
     return false;
   });
 });
+
+
+$(function () {
+  $("#how_it_works_carousel").on("slide", function (event) {
+    var allSlides = $(event.target).find(".item");
+    var slide = event.relatedTarget;
+    var index = Array.prototype.slice.call(allSlides).indexOf(slide);
+    $("#how_it_works_indicators li").removeClass("active");
+    $("#how_it_works_indicators li[data-slide-index=\"" + index + "\"]").addClass("active");
+  });
+});
