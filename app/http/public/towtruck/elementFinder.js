de9dbd37315217c3a5629151e542191ac13c3901
Ian Bicking <ianb@colorstudy.com>
2013-05-06 16:08:00 -0500

    Fix #374, fix error when using svg.className.indexOf.

--- app/http/public/towtruck/elementFinder.js
+++ app/http/public/towtruck/elementFinder.js
@@ -11,7 +11,7 @@ define(["util", "jquery"], function (util, $) {
       el = el.get(0);
     }
     while (el) {
-      if (el.className && (el.className == "towtruck" || el.className.indexOf(" towtruck") != -1 || el.className == "guider" || el.id == "guiders_overlay")) {
+      if (el.className && (el.className == "towtruck" || (el.className.indexOf && el.className.indexOf(" towtruck") != -1) || el.className == "guider" || el.id == "guiders_overlay")) {
         return true;
       }
       el = el.parentNode;
