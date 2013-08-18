ae5d6ebdaa07bd1da7af3829f5fbdaff9cb6decb
Aaron Druck <aldruck@gmail.com>
2013-08-17 18:47:07 -0700

    creating the new site

---
+++ site-reboot/js/custom.js
@@ -0,0 +1,10 @@
+
+//Video player
+
+$( "#video-area" ).click(function() {
+  $( "#marketing-video" ).fadeIn();
+});
+
+$( "#marketing-video" ).click(function() {
+  $( "#marketing-video" ).fadeOut();
+});
