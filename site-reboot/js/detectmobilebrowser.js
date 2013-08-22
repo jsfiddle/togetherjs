69bfbdfabc63f564aa692b795dc0ac01313091fb
Aaron Druck <aldruck@gmail.com>
2013-08-22 12:32:17 -0700

    detecting mobile device and stating up video

---
+++ site-reboot/js/detectmobilebrowser.js
@@ -0,0 +1,33 @@
+// detect a mobile device
+
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+if( isMobile.any() ){
+  
+  $( "#marketing-video" ).click(function() {
+    //auto play video
+    window.location = "http://www.google.com";
+  });
+  
+  //alert('test');
+  
+};
