5fd4fc093a3c11e017f12155e85601c038d2a395
Aaron Druck <aldruck@gmail.com>
2013-08-26 15:30:39 -0700

    finished all the animations

--- site/js/how-animations.js
+++ site/js/how-animations.js
@@ -2,31 +2,34 @@
     $(function(){
       
       //animate Why use ConnectJS?
-      
       $('#tour').waypoint(function() {
-          //animate this...
-          $( ".why-connect-01 img" ).animate({
-              top: "0",
-              opacity: 1
-            }, 300).queue(function(){
-              // then this...
-              $( ".why-connect-02 img" ).animate({
-                  top: "0",
-                  opacity: 1
-                }, 300).queue(function(){ 
-                  // then this...
-                  $( ".why-connect-03 img" ).animate({
-                      top: "0",
-                      opacity: 1
-                    }, 300);
-                  });
-              });
-        }, { offset: 300, triggerOnce: true });
+          setTimeout(function() {
+            $( ".why-connect-01 img" ).animate({
+                top: "0",
+                opacity: 1
+              }, 300);
+          }, 0);
+          
+          setTimeout(function() {
+            $( ".why-connect-02 img" ).animate({
+                top: "0",
+                opacity: 1
+              }, 300);
+          }, 100);
+          
+          setTimeout(function() {
+            $( ".why-connect-03 img" ).animate({
+                top: "0",
+                opacity: 1
+              }, 300);
+          }, 200);
 
+        }, { offset: 300, triggerOnce: true });
+  
         //animate How does ConnectJS work?
         $('.howto-animation-01').waypoint(function() {
           
-          // Add ConnectJS Javascript and HTML to your site.
+          // 1. Add ConnectJS Javascript and HTML to your site.
           $('.how-section-imganim-01').animate({
             left: "350"
           }, 800).animate({
@@ -38,21 +41,42 @@
           }, 800).animate({
               opacity: "0"
             }, 800).queue(function(){ 
-              
               //animate in the button
-              alert("animate button");
-              
+              $('.how-section-imganim-03').fadeIn();
               });
           
         }, { offset: 300, triggerOnce: true });
         
-        $('.howto-animation-02').waypoint(function() {
-          console.log("animation 2");
-        });
+        // 2. Your site is now ConnectJS enabled.
+        $('.howto-animation-02').waypoint(function() {   
+          $(".how-section-btncollab").fadeOut();  
+          $(".how-section-btncollabpressed").fadeIn();
+          setTimeout(function() {
+            $(".how-section-dockplacement").fadeIn();     
+          }, 1000);        
+        }, { offset: 400, triggerOnce: true });
         
+        // 3. Your site is now ConnectJS enabled.        
         $('.howto-animation-03').waypoint(function() {
+          
+          $('.cursor-placement-01').animate({
+            left: "-=15"
+          }, 800);
+          $('.cursor-placement-03').animate({
+            left: "-=30"
+          }, 800);
+          setTimeout(function() {
+             $('.cursor-placement-02').animate({
+               top: "-=40"
+             }, 800);
+             $('.cursor-placement-04').animate({
+               top: "-=15"
+             }, 800);  
+               
+           }, 1000);
+          
           console.log("animation 3");
-        });
+        }, { offset: 500, triggerOnce: true });
           
     })
 }(window.jQuery)
