071b88acafa3d3fe40be88398e7c517102fd6d74
Aaron Druck <aldruck@gmail.com>
2013-08-26 12:24:40 -0700

    working on the how animations

--- site/js/how-animations.js
+++ site/js/how-animations.js
@@ -1,28 +1,50 @@
 !function ($) {
     $(function(){
       
-        $('#tour').waypoint(function() {
-                 
+      //animate Why use ConnectJS?
+      
+      $('#tour').waypoint(function() {
+          //animate this...
           $( ".why-connect-01 img" ).animate({
               top: "0",
               opacity: 1
-            }, 500);
-        
-          $( ".why-connect-02 img" ).animate({
-              top: "0",
-              opacity: 1
-            }, 500);
+            }, 300).queue(function(){
+              // then this...
+              $( ".why-connect-02 img" ).animate({
+                  top: "0",
+                  opacity: 1
+                }, 300).queue(function(){ 
+                  // then this...
+                  $( ".why-connect-03 img" ).animate({
+                      top: "0",
+                      opacity: 1
+                    }, 300);
+                  });
+              });
+        }, { offset: 300, triggerOnce: true });
 
-          $( ".why-connect-03 img" ).animate({
-              top: "0",
-              opacity: 1
-            }, 500);
-          
-        }, { offset: 200, triggerOnce: true });
-
+        //animate How does ConnectJS work?
         $('.howto-animation-01').waypoint(function() {
-          console.log("animation 1");
-        });
+          
+          // Add ConnectJS Javascript and HTML to your site.
+          $('.how-section-imganim-01').animate({
+            left: "350"
+          }, 800).animate({
+              opacity: "0"
+            }, 800);
+            
+          $('.how-section-imganim-02').animate({
+            left: "350"
+          }, 800).animate({
+              opacity: "0"
+            }, 800).queue(function(){ 
+              
+              //animate in the button
+              alert("animate button");
+              
+              });
+          
+        }, { offset: 300, triggerOnce: true });
         
         $('.howto-animation-02').waypoint(function() {
           console.log("animation 2");
