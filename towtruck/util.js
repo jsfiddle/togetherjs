e6b788e46ff1f24606aed8fba2d30c17cb5df1f1
micmarsh <michael.marsh42@gmail.com>
2013-08-31 20:01:48 -0400

    upgrade to underscore's throttle

--- towtruck/util.js
+++ towtruck/util.js
@@ -253,16 +253,31 @@ define(["jquery", "jqueryPlugins", "jqueryui", "jquerypunch"], function ($) {
     });
   };
 
-  util.throttle = function (func, wait) {
+  util.throttle = function (func, wait, options) {
+    var context, args, result;
+    var timeout = null;
+    var previous = 0;
+    options || (options = {});
+    var later = function() {
+      previous = options.leading === false ? 0 : new Date;
+      timeout = null;
+      result = func.apply(context, args);
+    };
     return function() {
-        var _this = this,
-            args = [].slice(arguments);
-
-        clearTimeout(func._throttleTimeout);
-
-        func._throttleTimeout = setTimeout(function() {
-            func.apply(_this, args);
-        }, wait);
+      var now = new Date;
+      if (!previous && options.leading === false) previous = now;
+      var remaining = wait - (now - previous);
+      context = this;
+      args = arguments;
+      if (remaining <= 0) {
+        clearTimeout(timeout);
+        timeout = null;
+        previous = now;
+        result = func.apply(context, args);
+      } else if (!timeout && options.trailing !== false) {
+        timeout = setTimeout(later, remaining);
+      }
+      return result;
     };
   }
 
