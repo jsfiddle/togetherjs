cfd94fc5f2d8d9ee29a55020902acd8ff836587e
Aaron Druck <aldruck@gmail.com>
2013-09-20 11:37:32 -0700

    updating the scrollnav
    now when you click Get Together, it scrolls down

--- site/js/scrollTo.js
+++ site/js/scrollTo.js
@@ -15,15 +15,15 @@
         var $root = $('html, body');
 
         // unique nav tag here for HOME PAGE
-        // $('a.scrollnav').click(function() {
-        //     var href = $.attr(this, 'href');
-        //     $root.animate({
-        //         scrollTop: $(href).offset().top - 50
-        //     }, 500, function () {
-        //         window.location.hash = href;
-        //     });
-        //     return false;
-        // });
+        $('a.scrollnav').click(function() {
+            var href = $.attr(this, 'href');
+            $root.animate({
+                scrollTop: $(href).offset().top - 50
+            }, 500, function () {
+                window.location.hash = href;
+            });
+            return false;
+        });
         
         // unique nav tag here for DOCUMENATION page
         $('a.scrollnavdocs').click(function() {
