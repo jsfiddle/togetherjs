5164f3c82299a962c90b4bf5b0d65f958f4e1d36
Aaron Druck <aldruck@gmail.com>
2013-09-26 11:07:12 -0700

    updating some styles

--- site/js/parallax.js
+++ site/js/parallax.js
@@ -1,4 +1,27 @@
 $(document).ready(function(){
+  
+  // detect a mobile device
+  var isMobile = {
+      Android: function() {
+          return navigator.userAgent.match(/Android/i);
+      },
+      BlackBerry: function() {
+          return navigator.userAgent.match(/BlackBerry/i);
+      },
+      iOS: function() {
+          return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+      },
+      Opera: function() {
+          return navigator.userAgent.match(/Opera Mini/i);
+      },
+      Windows: function() {
+          return navigator.userAgent.match(/IEMobile/i);
+      },
+      any: function() {
+          return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+      }
+  };
+  
 	// Cache the Window object
 	$window = $(window);
                 
@@ -13,6 +36,13 @@ $(document).ready(function(){
 		
 		// Put together our final background position
 		var coords = '50% '+ yPos + 'px';
+		
+		if (isMobile.any()) {
+		  //remove parallax
+		  var coords = '';
+		}
+		
+		
 
 		// Move the background
 		$bgobj.css({ backgroundPosition: coords });
