934f0ec21e84fbe06d08782627be1791d971862f
Simon Wex <simon@simonwex.com>
2013-10-08 11:56:09 -0400

    Adding devserver for collaborators. Fixes #831

---
+++ devserver.js
@@ -0,0 +1,38 @@
+var
+http = require("http"),
+url  = require("url"),
+path = require("path"),
+fs   = require("fs"),
+port = process.argv[2] || process.env['PORT'] || 8080;
+
+http.createServer(function(request, response) {
+
+  var uri = url.parse(request.url).pathname
+  var filename = path.join(process.cwd(), 'build', uri);
+
+  fs.exists(filename, function(exists) {
+    if(!exists) {
+      response.writeHead(404, {"Content-Type": "text/plain"});
+      response.write("404 Not Found\n");
+      response.end();
+      return;
+    }
+
+    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+    fs.readFile(filename, "binary", function(err, file) {
+      if(err) {
+        response.writeHead(500, {"Content-Type": "text/plain"});
+        response.write(err + "\n");
+        response.end();
+        return;
+      }
+
+      response.writeHead(200);
+      response.write(file, "binary");
+      response.end();
+    });
+  });
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
