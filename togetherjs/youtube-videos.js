4d11b1628c4d65cabc7f6dda41e6523d4de3df2f
maxsong1002 <maxsong1002@hotmail.com>
2013-10-27 11:59:58 +0900

    multiple issues fixed: a) videos are placed in the array in the right order b) do not send anything when state changes to buffering c) dontPublish shouldnt be set True when the player wouldnt change its state after receving a message

--- togetherjs/youtube-videos.js
+++ togetherjs/youtube-videos.js
@@ -18,7 +18,7 @@ function ($, util, session, elementFinder) {
   var iframes = $('iframe#youtube-player');
 
   // boolean to indicate when to not publish
-  var dontPublish = false;
+  var dontPublish = [];
 
   // TODO: enablejsapi should be set as well
   // the only thing the user has to do should be setting iframe id to "youtube-player"
@@ -44,62 +44,91 @@ function ($, util, session, elementFinder) {
 
   function insertPlayer(event) {
     // only when it is READY, insert each player into the list
-    players.push(event.target);
+    var currentPlayer = event.target;
+    // get playerIndex and insert it into that specific location
+    var playerIndex = getPlayerIndex(currentPlayer);
+    players[playerIndex] = currentPlayer;
+    dontPublish[playerIndex] = false;
   }
 
+  function getPlayerIndex(currentPlayer) {
+    // ex)element = <iframe id="youtube-player0" ...>
+    var element = currentPlayer.a;
+    // ex)whichPlayer = iframe#youtube-player0
+    var whichPlayer = elementFinder.elementLocation(element);
+    // ex)playerIndex = 0
+    var playerIndex = whichPlayer[whichPlayer.length-1];
+    return playerIndex;
+  }
   
   function publishPlayerStateChange(event) {
+    var currentPlayer = event.target;
+    var currentTime = currentPlayer.getCurrentTime();
+    var playerIndex = getPlayerIndex(currentPlayer);
+
     // do not publish if playerState was changed by other users
-    if (dontPublish) {
+    if (dontPublish[playerIndex]) {
       // make it false again so it can start publishing events of its own state changes
-      dontPublish = false;
+      dontPublish[playerIndex] = false;
       return;
     }
+
     // notify other people that I changed the player state
-    if (event.data == YT.PlayerState.PLAYING) {
-      var element = event.target.a;
+    if (event.data == YT.PlayerState.PLAYING) {      
       session.send({
         type: "playerStateChange",
+        playerIndex: playerIndex, // id of iframe that changed state
         playerState: 1,
-        whichPlayer: elementFinder.elementLocation(element) // id of iframe that changed state
+        playerTime: currentTime
+
       });
-    } else if (event.data == YT.PlayerState.PAUSED) {
-      var element = event.target.a;
+    } else if (event.data == YT.PlayerState.PAUSED) {      
       session.send({
         type: "playerStateChange",
+        playerIndex: playerIndex,
         playerState: 2,
-        whichPlayer: elementFinder.elementLocation(element) 
+        playerTime: currentTime,
+
       });
     } else if (event.data == YT.PlayerState.BUFFERING) {
-      var element = event.target.a;
-      session.send({
-        type: "playerStateChange",
-        playerState: 3,
-        whichPlayer: elementFinder.elementLocation(element) 
-      });
+      console.log("Im just buffering. Do nothing");
     } else if (event.data == YT.PlayerState.CUED) {
+      // TODO: should I syncrhonize newly loaded videos as well?
       //videoUrl = player.getVideoUrl();
     } else if (event.data == YT.PlayerState.ENDED) {
-      var element = event.target.a;
       session.send({
         type: "playerStateChange",
-        playerState: 0,
-        whichPlayer: elementFinder.elementLocation(element) 
+        playerIndex: playerIndex,
+        playerState: 0
       });
     }
   }
   
   session.hub.on('playerStateChange', function (msg) {
-    // ex)whichPlayer = iframe#youtube-player0
-    dontPublish = true;
-    var playerIndex = msg.whichPlayer[msg.whichPlayer.length-1];
-    var player = players[playerIndex];
+    var player = players[msg.playerIndex];
+    var currentTime = player.getCurrentTime();
+    var currentState = player.getPlayerState();
+
+    if (currentState != msg.playerState) {
+      dontPublish[msg.playerIndex] = true;
+    }
+
     if (msg.playerState == 1) {
-      // TODO: get time diff and advance if the diff is too large
+      if (areTooFarApart(currentTime, msg.playerTime)) {
+        player.seekTo(msg.playerTime);
+      }
       player.playVideo();
     } else if (msg.playerState == 2) {
-      // TODO: same as above
+      if (areTooFarApart(currentTime, msg.playerTime)) {
+        player.seekTo(msg.playerTime);
+      }
       player.pauseVideo();
     }
   });
+
+  function areTooFarApart(myTime, theirTime) {
+    var secDiff = Math.abs(myTime - theirTime);
+    var milliDiff = secDiff * 1000;
+    return milliDiff > TOO_FAR_APART;
+  }
 });
