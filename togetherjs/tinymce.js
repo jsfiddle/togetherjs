977609bc2b088bae3eacc7c35af1e8dc6a0a776d
maxsong1002 <maxsong1002@hotmail.com>
2013-11-07 10:31:19 +0900

    tinymce is synchronized, but it still needs major style fixes

--- togetherjs/tinymce.js
+++ togetherjs/tinymce.js
@@ -2,46 +2,61 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http:// mozilla.org/MPL/2.0/. */
 
-define(["jquery", "util", "session", "elementFinder"],
-function ($, util, session, elementFinder) {
+define(["jquery", "util", "session", "elementFinder", "ot"],
+function ($, util, session, elementFinder, ot) {
+  var assert = util.assert;
 
-  //TODO: check fog CONFIG
+  // id's are automatically set as mce_i (ex: mce_0) by tinymce
+  // no need to scan manually because tinymce.editors is sufficient
 
-  // user must set his all tinymce div classes to "tinymce"
-  // he MUST NOT set id's to tinymce divs
-  // id's are automatically set as mce_i (ex: mce_0)
+  // TODO: a lot of style changes are probably needed before it can be merged
+  // It might be necessary to follow the styles of AceEditor or CodeMirror
 
-  $(function () {
-    prepareTinymce();
+  session.on("reinitialize", function () {
+    if (TogetherJS.config.get("tinymce")) {
+      prepareTinymce();
+    }
+  });
+
+  TogetherJS.config.track("tinymce", function (track, previous) {
+    if (track && ! previous) {
+      prepareTinymce();
+      // You can enable tinymce dynamically, but can't turn it off:
+      TogetherJS.config.close("tinymce");
+    }
   });
   
   function prepareTinymce() {
     // check for a tinymce instance
     if (tinymce !== undefined) {
       // tinymce editors are done initialized by the time togetherjs finishes loading
-      // it is not necessary to listen to init or addEditor events
-      //setupTinymceLocations();
+      // it is not necessary to listen to init or addEditor events on init
       var editors = tinymce.editors;
-      $(editors).each(function (i, editor) {
-        // clean up the editor
-        editor.setContent("");
-        // record what the user is typing
-        editor.on("keypress", function (event) {
-          /*
-          var keyCode = event.keyCode ? event.keyCode : event.charCode;
-          // publish this keyCode
-          publishKeyCode(editor, keyCode);
-          */
-          var content = editor.getContent({format: 'raw'});
-          publishContent(editor, content);
-        });
-      });
+      setEventListeners(editors); // those with events listeners will be ignored
     }
   }
 
+  function setEventListeners(editors) {
+    $(editors).each(function (i, editor) {
+      // record what the user is typing
+      editor.on("keypress", function (event) {
+        var content = editor.getContent();
+        publishContent(editor, content);
+      });
+      // this event is fired when the content is changed("bold, italicized, delete or paste")
+      // however, it is not invoked for every new character so we must have both event handlers
+      editor.on("change", function (event) {
+        var content = editor.getContent();
+        publishContent(editor, content);
+      });
+    });
+  }
+
   function publishContent(editor, content) {
-    // FIXME: this will get very slow as the size of content becomes large
-    var location = elementFinder.elementLocation(editor); // or just tinymce.get('mce_id')
+    // FIXME: what if two people type at the same time?
+    // it seems necessary to use ot.SimpleHistory to solve such problem
+    // because SimpleHistory serializes the order of updates
+    var location = elementFinder.elementLocation(editor); // passing ('mce_id') is sufficient
     session.send({
       type: "contentChange",
       element: location,
@@ -49,39 +64,41 @@ function ($, util, session, elementFinder) {
     });
   }
 
-  function publishKeyCode(editor, keyCode) {
-    var location = elementFinder.elementLocation(editor);
-    session.send({
-      type: "pressedKeyboard",
-      element: location,
-      keyCode: keyCode
+  session.hub.on('hello', function () {
+    var editors = tinymce.editors;
+    editors.forEach(function (editor) {
+      var content = editor.getContent();
+      publishContent(editor, content);
     });
-  }
+  });
 
+  /*
+  function getChangedContent() {
+    currentContent = editor.getContent();
+    var delta = ot.TextReplace.fromChange(history.current, editor.getContent());
+  }
+  */
+
+  // "replace" my content with the latest content
   session.hub.on("contentChange", function (msg) {
     var element = elementFinder.findElement(msg.element);
     var editor = tinymce.get(element.id);
+    // I could compare currentContent and msg.content, but calling the api function would
+    // be as much costly as just doing setContent everytime I receive a contentChange event
     editor.setContent(msg.content, {format: 'raw'});
   })
 
-  session.hub.on("pressedKeyboard", function (msg) {
-    var element = elementFinder.findElement(msg.element);
-    var editor = tinymce.get(element.id);
-    var letter = String.fromCharCode(msg.keyCode);
-    if (letter === undefined) {
-      // FIXME: trigger doesnt work in tinymce editor
-      editor.trigger({
-        type: 'keypress',
-        which: msg.keyCode,
-        keyCode: msg.keyCode
-      });
-    } else {
-      var currentContent = editor.getContent({format: 'raw'});
-      editor.setContent(currentContent + letter, {format: 'raw'});
-    }
-
+  // get rid of event listeners upon closing togetherjs
+  session.on("close", function () {
+    var editors = tinymce.editors;
+    editors.off("keypress");
+    editors.off("change");
   });
-
-
-
 }); // END OF MODULE
+
+/*
+var history = $(editor).data("togetherjsHistory");
+var delta = ot.TextReplace.fromChange(history.current, content);
+assert(delta);
+history.add(delta);
+*/
