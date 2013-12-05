3b30c492fe6fed30ec42ebf4e0e2701e06888baa
maxsong1002 <maxsong1002@hotmail.com>
2013-12-05 11:14:59 +0900

    youtube module fix: onStateChange event target is not a complete player object. A player is retrieved directly from the iframe instead of the event

--- site/examples/youtube/js/application.js
+++ site/examples/youtube/js/application.js
@@ -2,10 +2,12 @@ $(function () {
   $('.embed-submit-button').click(function() {
     // FIXME: uncaught exception: [CannotFind #video-id-input(:nth-child(1)): container only has 0 elements in #video-id-input]
     // I am not sure where this exception is generated. Maybe it is caused by togetherJS?
-    var newVideoId = $('video-id-input').val();
+    var newVideoId = $('#video-id-input').val();
     var newSrc = '//www.youtube.com/embed/' + newVideoId;
     var youTubeIframe = $('iframe')[0];
     $(youTubeIframe).attr("src", newSrc);
+    console.log("gonna run reinitailize now...");
+    //reinitialize to configure youtube players again
     TogetherJS.reinitialize();
   });
   $('video-id-input').keypress(function(event) {
