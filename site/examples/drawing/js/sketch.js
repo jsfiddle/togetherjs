533ed3bd8285f3ebd48d9dedbe89b3abd0f5d3cf
Martin Ek <ekmartinb@gmail.com>
2013-12-09 13:13:57 +0100

    Add button css

--- site/examples/drawing/js/sketch.js
+++ site/examples/drawing/js/sketch.js
@@ -31,43 +31,66 @@ canvas.addEventListener('mouseup', function () {
   canvas.removeEventListener('mousemove', move, false);
 }, false);
 
+// Sets the brush size:
 function setSize(size) {
   context.lineWidth = size;
 }
 
+// Sets the brush color:
 function setColor(color) {
   context.globalCompositeOperation = "source-over";
   context.strokeStyle = color;
 }
 
+// Sets the brush to erase-mode:
 function eraser() {
 context.globalCompositeOperation = "destination-out";
 context.strokeStyle = "rgba(0,0,0,1)";
 }
+
+// Draws the lines, called by move and the TogetherJS event listener:
+function draw(start, end, color, size, compositeOperation) {
+    context.save();
+    context.strokeStyle = color;
+    context.globalCompositeOperation = compositeOperation;
+    context.lineWidth = size;
+    context.beginPath();
+    context.moveTo(start.x, start.y);
+    context.lineTo(end.x, end.y);
+    context.closePath();
+    context.stroke();
+    context.restore();
+}
+
+// Called whenever the mousemove event is fired, calls the draw function:
 function move(e) {
-  var mouse = {
-    x: e.pageX - this.offsetLeft,
-    y: e.pageY - this.offsetTop
-  };
-  draw(lastMouse, mouse);
-  lastMouse = mouse;
+    var mouse = {
+        x: e.pageX - this.offsetLeft,
+        y: e.pageY - this.offsetTop
+    };
+    draw(lastMouse, mouse, context.strokeStyle, context.lineWidth, context.globalCompositeOperation);
+    if (TogetherJS.running) {
+        TogetherJS.send({
+            type: "draw",
+            start: lastMouse,
+            end: mouse,
+            color: context.strokeStyle,
+            size: context.lineWidth,
+            compositeOperation: context.globalCompositeOperation
+        });
+    }
+    lastMouse = mouse;
 }
 
-function draw(start, end, remote) {
-  context.beginPath();
-  context.moveTo(start.x, start.y);
-  context.lineTo(end.x, end.y);
-  context.closePath();
-  context.stroke();
-  if ((! remote) && TogetherJS.running) {
-      TogetherJS.send({type: "draw", start: start, end: end});
-  }
-}
-
+// Listens for draw messages, sends info about the drawn lines:
 TogetherJS.hub.on("draw", function (msg) {
-  draw(msg.start, msg.end, true);
+    if (!msg.sameUrl) {
+        return;
+    }
+    draw(msg.start, msg.end, msg.color, msg.size, msg.compositeOperation);
 });
 
+// Hello is sent from every newly connected user, this way they will receive what has already been drawn:
 TogetherJS.hub.on("togetherjs.hello", function () {
   var image = canvas.toDataURL("image/png");
   TogetherJS.send({
@@ -76,84 +99,92 @@ TogetherJS.hub.on("togetherjs.hello", function () {
   });
 });
 
+// Draw initially received drawings:
 TogetherJS.hub.on("init", function (msg) {
   var image = new Image();
   image.src = msg.image;
   context.drawImage(image, 0, 0);
 });
 
+// JQuery to handle buttons, also changes the cursor to a dot resembling the brush size:
 $(document).ready(function () {
-function changeMouse() {
-  var cursorSize = context.lineWidth;
-  if (cursorSize < 10){
-      cursorSize = 10;
+  // changeMouse creates a temporary invisible canvas that shows the cursor, which is then set as the cursor through css:
+  function changeMouse() {
+    var cursorSize = context.lineWidth;
+    if (cursorSize < 10){
+        cursorSize = 10;
+    }
+    var cursorColor = context.strokeStyle;
+    var cursorGenerator = document.createElement("canvas");
+    cursorGenerator.width = cursorSize;
+    cursorGenerator.height = cursorSize;
+    var ctx = cursorGenerator.getContext("2d");
+
+    var centerX = cursorGenerator.width/2;
+    var centerY = cursorGenerator.height/2;
+
+    ctx.beginPath();
+    ctx.arc(centerX, centerY, (cursorSize/2)-4, 0, 2 * Math.PI, false);
+
+    // If the user is erasing, set the fill of the cursor to white.
+    if (context.globalCompositeOperation == 'destination-over') {
+         ctx.fillStyle = 'white';
+         ctx.fill();
+    }
+
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = cursorColor;
+    ctx.stroke();
+    console.log(context.lineWidth, cursorSize);
+    $('#sketch').css( "cursor", "url(" + cursorGenerator.toDataURL("image/png") + ") " + cursorSize/2 + " " + cursorSize/2 + ",crosshair");
   }
-  var cursorColor = context.strokeStyle;
-  var cursorGenerator = document.createElement("canvas");
-  cursorGenerator.width = cursorSize;
-  cursorGenerator.height = cursorSize;
-  var ctx = cursorGenerator.getContext("2d");
-
-  var centerX = cursorGenerator.width/2;
-  var centerY = cursorGenerator.height/2;
-
-  ctx.beginPath();
-  ctx.arc(centerX, centerY, (cursorSize/2)-4, 0, 2 * Math.PI, false);
-
-  // If the user is erasing, set the fill of the cursor to white.
-  if (context.globalCompositeOperation == 'destination-over') {
-       ctx.fillStyle = 'white';
-       ctx.fill();
-  }
-
-  ctx.lineWidth = 3;
-  ctx.strokeStyle = cursorColor;
-  ctx.stroke();
-  $('#sketch').css( "cursor", "url(" + cursorGenerator.toDataURL("image/png") + ") " + cursorSize/2 + " " + cursorSize/2 + ",crosshair");
-}
-// Init mouse
-changeMouse();
-// Color change functions:
-$('.green-pick').click(function () {
-  setColor('#00ff00');
+  // Init mouse
   changeMouse();
-});
+  // Color-button functions:
+  $('.green-pick').click(function () {
+    setColor('#00ff00');
+    changeMouse();
+  });
 
-$('.yellow-pick').click(function () {
-  setColor('#ff0');
-  changeMouse();
-});
+  $('.yellow-pick').click(function () {
+    setColor('#ff0');
+    changeMouse();
+  });
 
-$('.red-pick').click(function () {
-  setColor('#ff0000');
-  changeMouse();
-});
+  $('.red-pick').click(function () {
+    setColor('#ff0000');
+    changeMouse();
+  });
 
-$('.blue-pick').click(function () {
-  setColor('#0000ff');
-  changeMouse();
-});
+  $('.blue-pick').click(function () {
+    setColor('#0000ff');
+    changeMouse();
+  });
 
-$('.black-pick').click(function () {
-  setColor('#000');
-  changeMouse();
-});
-$('.eraser').click(function () {
-  eraser();
-  changeMouse();
-});
-// TogetherJS user color:
-$('.user-color-pick').click(function() {
-  setColor(TogetherJS.require("peers").Self.color);
-  changeMouse();
-});
+  $('.black-pick').click(function () {
+    setColor('#000');
+    changeMouse();
+  });
+  $('.eraser').click(function () {
+    eraser();
+    changeMouse();
+  });
+  // TogetherJS user color:
+  $('.user-color-pick').click(function() {
+    setColor(TogetherJS.require("peers").Self.color);
+    changeMouse();
+  });
 
-// Increase/decrease brush size:
-$('.plus-size').click(function() {
-  setSize(context.lineWidth+1);
-});
+  // Increase/decrease brush size:
+  $('.plus-size').click(function() {
+    setSize(context.lineWidth+3);
+    changeMouse();
+  });
 
-$('.minus-size').click(function() {
-  setSize(context.lineWidth-1);
-});          
+  $('.minus-size').click(function() {
+    if (context.lineWidth > 3) {
+      setSize(context.lineWidth-3);
+    }
+    changeMouse();
+  });          
 });
