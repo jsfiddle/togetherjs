9562ddf2aaef14056fbe134679227f86a22d918f
Howon Song <hs459@cornell.edu>
2014-09-06 16:21:52 -0400

    #1033 - Instead of changing youtube src and reinitializing, just load by videoId

--- site/examples/youtube/js/application.js
+++ site/examples/youtube/js/application.js
@@ -3,12 +3,16 @@ $(function () {
     // FIXME: uncaught exception: [CannotFind #video-id-input(:nth-child(1)): container only has 0 elements in #video-id-input]
     // I am not sure where this exception is generated. Maybe it is caused by togetherJS?
     var newVideoId = $('#video-id-input').val();
-    var newSrc = '//www.youtube.com/embed/' + newVideoId;
-    var youTubeIframe = $('iframe')[0];
-    $(youTubeIframe).attr("src", newSrc);
-    console.log("gonna run reinitailize now...");
-    //reinitialize to configure youtube players again
-    TogetherJS.reinitialize();
+    var $youTubeIframe = $("iframe[src*='youtube']");
+    var player = $youTubeIframe.data('togetherjs-player');
+    player.loadVideoById(newVideoId);
+
+    // If the iframe's src is changed, the saved youtube player malfunctions
+    
+    // $(youTubeIframe).attr("src", newSrc);
+    // console.log("gonna run reinitailize now...");
+    // //reinitialize to configure youtube players again
+    // TogetherJS.reinitialize();
   });
   $('video-id-input').keypress(function(event) {
     if (event.keyCode == 13)
