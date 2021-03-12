2fc2794d3c5eb84b979cd219d40b834fd4e6370c
Anonymous <anon@ymo.us>
2021-03-12 19:33:26 +0100

    convert eventMaker.ts

--- togetherjs/ts/eventMaker.ts
+++ togetherjs/ts/eventMaker.ts
@@ -1,60 +1,67 @@
-"use strict";
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+interface TogetherjsMouseEvent extends MouseEvent {
+    togetherjsInternal: true;
+}
+
 function createTogetherjsMouseEvent() {
-    var event = document.createEvent("MouseEvents");
+    const event = document.createEvent("MouseEvents") as TogetherjsMouseEvent;
     // FIXME: I'm not sure this custom attribute always propagates?
     // seems okay in Firefox/Chrome, but I've had problems with
     // setting attributes on keyboard events in the past.
     event.togetherjsInternal = true;
     return event;
 }
-define(["jquery", "util"], function ($, util) {
-    var eventMaker = /** @class */ (function () {
-        function EventMaker() {
-        }
-        EventMaker.prototype.performClick = function (target) {
+
+define(["jquery", "util"], function($: JQueryStatic, util: Util) {
+    let eventMaker = class EventMaker {
+
+        performClick(target: HTMLElement | JQuery) {
             // FIXME: should accept other parameters, like Ctrl/Alt/etc
             var event = createTogetherjsMouseEvent();
-            event.initMouseEvent("click", // type
-            true, // canBubble
-            true, // cancelable
-            window, // view
-            0, // detail
-            0, // screenX
-            0, // screenY
-            0, // clientX
-            0, // clientY
-            false, // ctrlKey
-            false, // altKey
-            false, // shiftKey
-            false, // metaKey
-            0, // button
-            null // relatedTarget
+            event.initMouseEvent(
+                "click", // type
+                true, // canBubble
+                true, // cancelable
+                window, // view
+                0, // detail
+                0, // screenX
+                0, // screenY
+                0, // clientX
+                0, // clientY
+                false, // ctrlKey
+                false, // altKey
+                false, // shiftKey
+                false, // metaKey
+                0, // button
+                null // relatedTarget
             );
+
             target = $(target)[0];
             var cancelled = target.dispatchEvent(event);
-            if (cancelled) {
+            if(cancelled) {
                 return;
             }
-            if (target.tagName == "A") {
-                var href = target.href;
-                if (href) {
+            if(target.tagName == "A") {
+                var href = (target as HTMLAnchorElement).href;
+                if(href) {
                     location.href = href;
                     return;
                 }
             }
             // FIXME: should do button clicks (like a form submit)
             // FIXME: should run .onclick() as well
-        };
-        EventMaker.prototype.fireChange = function (target) {
+        }
+
+        fireChange(target: HTMLElement | JQuery) {
             target = $(target)[0];
             var event = document.createEvent("HTMLEvents");
             event.initEvent("change", true, true);
             target.dispatchEvent(event);
-        };
-        return EventMaker;
-    }());
+        }
+    }
+
     return eventMaker;
 });
