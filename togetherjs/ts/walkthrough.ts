0e53c06b121ef43c8bc04ccd1192983764c5668d
Anonymous <anon@ymo.us>
2021-03-21 12:55:59 +0100

    convert walkthrough

--- togetherjs/ts/walkthrough.ts
+++ togetherjs/ts/walkthrough.ts
@@ -4,87 +4,10 @@
 
 function walkthroughMain(util: Util, ui, $: JQueryStatic, windowing: TogetherJSNS.Windowing, templates: TogetherJSNS.Templates, templating: TogetherJSNS.Templating, session: TogetherJSNS.Session, peers: TogetherJSNS.Peers) {
     var assert = util.assert;
-    var walkthrough = util.Module("walkthrough");
     var onHideAll = null;
     var container = null;
 
     var slides = null;
-
-    walkthrough.start = function(firstTime, doneCallback) {
-        if(!container) {
-            container = $(templates("walkthrough"));
-            container.hide();
-            ui.container.append(container);
-            slides = container.find(".togetherjs-walkthrough-slide");
-            slides.hide();
-            var progress = $("#togetherjs-walkthrough-progress");
-            slides.each(function(index) {
-                var bullet = templating.sub("walkthrough-slide-progress", {});
-                progress.append(bullet);
-                bullet.click(function() {
-                    show(index);
-                });
-            });
-            container.find("#togetherjs-walkthrough-previous").click(previous);
-            container.find("#togetherjs-walkthrough-next").click(next);
-            ui.prepareShareLink(container);
-            container.find(".togetherjs-self-name").bind("keyup", function(event) {
-                var val = $(event.target).val();
-                peers.Self.update({ name: val });
-            });
-            container.find(".togetherjs-swatch").click(function() {
-                var picker = $("#togetherjs-pick-color");
-                if(picker.is(":visible")) {
-                    picker.hide();
-                    return;
-                }
-                picker.show();
-                picker.find(".togetherjs-swatch-active").removeClass("togetherjs-swatch-active");
-                picker.find(".togetherjs-swatch[data-color=\"" + peers.Self.color + "\"]").addClass("togetherjs-swatch-active");
-                var location = container.find(".togetherjs-swatch").offset();
-                picker.css({
-                    top: location.top,
-                    // The -7 comes out of thin air, but puts it in the right place:
-                    left: location.left - 7
-                });
-            });
-            if(session.isClient) {
-                container.find(".togetherjs-if-creator").remove();
-                container.find(".togetherjs-ifnot-creator").show();
-            } else {
-                container.find(".togetherjs-if-creator").show();
-                container.find(".togetherjs-ifnot-creator").remove();
-            }
-            TogetherJS.config.track("siteName", function(value) {
-                value = value || document.title;
-                container.find(".togetherjs-site-name").text(value);
-            });
-            ui.activateAvatarEdit(container, {
-                onSave: function() {
-                    container.find("#togetherjs-avatar-when-saved").show();
-                    container.find("#togetherjs-avatar-when-unsaved").hide();
-                },
-                onPending: function() {
-                    container.find("#togetherjs-avatar-when-saved").hide();
-                    container.find("#togetherjs-avatar-when-unsaved").show();
-                }
-            });
-            // This triggers substititions in the walkthrough:
-            peers.Self.update({});
-            session.emit("new-element", container);
-        }
-        assert(typeof firstTime == "boolean", "You must provide a firstTime boolean parameter");
-        if(firstTime) {
-            container.find(".togetherjs-walkthrough-firsttime").show();
-            container.find(".togetherjs-walkthrough-not-firsttime").hide();
-        } else {
-            container.find(".togetherjs-walkthrough-firsttime").hide();
-            container.find(".togetherjs-walkthrough-not-firsttime").show();
-        }
-        onHideAll = doneCallback;
-        show(0);
-        windowing.show(container);
-    };
 
     function show(index) {
         slides.hide();
@@ -132,13 +55,95 @@ function walkthroughMain(util: Util, ui, $: JQueryStatic, windowing: TogetherJSN
         return 0;
     }
 
-    walkthrough.stop = function() {
-        windowing.hide(container);
-        if(onHideAll) {
-            onHideAll();
-            onHideAll = null;
+    class Walkthrough {
+
+        start(firstTime, doneCallback) {
+            if(!container) {
+                container = $(templates("walkthrough"));
+                container.hide();
+                ui.container.append(container);
+                slides = container.find(".togetherjs-walkthrough-slide");
+                slides.hide();
+                var progress = $("#togetherjs-walkthrough-progress");
+                slides.each(function(index) {
+                    var bullet = templating.sub("walkthrough-slide-progress", {});
+                    progress.append(bullet);
+                    bullet.click(function() {
+                        show(index);
+                    });
+                });
+                container.find("#togetherjs-walkthrough-previous").click(previous);
+                container.find("#togetherjs-walkthrough-next").click(next);
+                ui.prepareShareLink(container);
+                container.find(".togetherjs-self-name").bind("keyup", function(event) {
+                    var val = $(event.target).val();
+                    peers.Self.update({ name: val });
+                });
+                container.find(".togetherjs-swatch").click(function() {
+                    var picker = $("#togetherjs-pick-color");
+                    if(picker.is(":visible")) {
+                        picker.hide();
+                        return;
+                    }
+                    picker.show();
+                    picker.find(".togetherjs-swatch-active").removeClass("togetherjs-swatch-active");
+                    picker.find(".togetherjs-swatch[data-color=\"" + peers.Self.color + "\"]").addClass("togetherjs-swatch-active");
+                    var location = container.find(".togetherjs-swatch").offset();
+                    picker.css({
+                        top: location.top,
+                        // The -7 comes out of thin air, but puts it in the right place:
+                        left: location.left - 7
+                    });
+                });
+                if(session.isClient) {
+                    container.find(".togetherjs-if-creator").remove();
+                    container.find(".togetherjs-ifnot-creator").show();
+                } else {
+                    container.find(".togetherjs-if-creator").show();
+                    container.find(".togetherjs-ifnot-creator").remove();
+                }
+                TogetherJS.config.track("siteName", function(value) {
+                    value = value || document.title;
+                    container.find(".togetherjs-site-name").text(value);
+                });
+                ui.activateAvatarEdit(container, {
+                    onSave: function() {
+                        container.find("#togetherjs-avatar-when-saved").show();
+                        container.find("#togetherjs-avatar-when-unsaved").hide();
+                    },
+                    onPending: function() {
+                        container.find("#togetherjs-avatar-when-saved").hide();
+                        container.find("#togetherjs-avatar-when-unsaved").show();
+                    }
+                });
+                // This triggers substititions in the walkthrough:
+                peers.Self.update({});
+                session.emit("new-element", container);
+            }
+            assert(typeof firstTime == "boolean", "You must provide a firstTime boolean parameter");
+            if(firstTime) {
+                container.find(".togetherjs-walkthrough-firsttime").show();
+                container.find(".togetherjs-walkthrough-not-firsttime").hide();
+            } else {
+                container.find(".togetherjs-walkthrough-firsttime").hide();
+                container.find(".togetherjs-walkthrough-not-firsttime").show();
+            }
+            onHideAll = doneCallback;
+            show(0);
+            windowing.show(container);
         }
-    };
+
+        stop() {
+            windowing.hide(container);
+            if(onHideAll) {
+                onHideAll();
+                onHideAll = null;
+            }
+        }
+
+    }
+
+    const walkthrough = new Walkthrough();
 
     session.on("hide-window", function() {
         if(onHideAll) {
