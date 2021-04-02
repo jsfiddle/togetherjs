980b2750e27d309160a92a01dd3613c0e62fd539
Anonymous <anon@ymo.us>
2021-04-02 10:24:36 +0200

    fix some export with a new syntax and remove main.ts file

--- togetherjs/templates-localized.js
+++ togetherjs/templates-localized.js
@@ -1,11 +1,12 @@
-"use strict";
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
-define([], function () {
-    return {
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    var tpl = {
         "interface": "__interface_html__",
         walkthrough: "__walkthrough_html__",
         names: "__names__"
     };
+    return tpl;
 });
