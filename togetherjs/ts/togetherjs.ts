28fe5b0b75a330ef5f68494cb5d158a70658b44a
Anonymous <anon@ymo.us>
2021-04-10 18:28:03 +0200

    disable no-var rule for TogetherJS declaration

--- togetherjs/ts/togetherjs.ts
+++ togetherjs/ts/togetherjs.ts
@@ -3,6 +3,7 @@ License, v. 2.0. If a copy of the MPL was not distributed with this file,
 You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 
+// eslint-disable-next-line no-var
 var TogetherJS: TogetherJSNS.TogetherJSClass = togetherjsMain();
 
 class OnClass<Map extends {[messageName: string]: TogetherJSNS.CallbackForOn<void>}> {
